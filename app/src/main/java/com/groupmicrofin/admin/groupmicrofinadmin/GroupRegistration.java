af384383c8df9b019065328f19ddfaa151a87a9a
groupmicrofin <groupmicrofin@gmail.com>
2018-07-29 20:26:01 +0530

    29-07-17: Admin Application BootStrapped

---
+++ app/src/main/java/com/groupmicrofin/admin/groupmicrofinadmin/GroupRegistration.java
@@ -0,0 +1,14 @@
+package com.groupmicrofin.admin.groupmicrofinadmin;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GroupRegistration extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_registration);
+    }
+}
